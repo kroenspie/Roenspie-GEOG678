e27cfb31e4fbb315e25a42d6a6dcbb72cc7f3ab1
kroenspie <kroenspie@tamu.edu>
2025-04-28 15:34:23 -0500

    Create theJSON.js

---
+++ Labs/Week04/Part1/theJSON.js
@@ -0,0 +1,22 @@
+//data.js is a file that contains the actual student data for the analysis
+var theJSON =  
+{
+    "data":
+    [
+        {
+            "name": "dan",
+            "age": 39,
+            "gpa": 4.0
+        },
+        {
+            "name": "paul",
+            "age": 55,
+            "gpa": 3.0
+        },
+        {
+            "name": "jason",
+            "age": 25,
+            "gpa": 2.0
+        }
+    ]
+}
